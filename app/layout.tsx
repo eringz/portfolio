8aa5ef0667fb85f25a7e413e3f99a8222689fb3e
unknown <ron.garcia.santos@gmail.com>
2024-10-21 23:24:54 +0800

    ready to deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,16 +12,16 @@ import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 const jetbrainsMono = JetBrains_Mono({
   subsets:  ['latin'],
