97fb57a9573f5c278d6f1d1c880e55f7b876a904
unknown <ron.garcia.santos@gmail.com>
2025-09-20 04:21:18 +0800

    Adjustment of projects page background

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -5,8 +7,25 @@ import { FiDownload } from "react-icons/fi";
 import Social from "@/components/ui/Social";
 import Photo from "@/components/ui/Photo";
 import Stats from "@/components/ui/Stats";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const Home = () => {
+    const [greeting, setGreeting] = useState("Hello");
+
+    useEffect(() => {
+      const hour = new Date().getHours();
+      if (hour < 12) setGreeting("Good Morning");
+      else if (hour < 18) setGreeting("Good Afternoon");
+      else if (hour == 24) setGreeting("Hey its Midnight");
+      else setGreeting("Good Evening");
+    }, []);
+
+    const text = `${greeting}! I'm`;
+    const name = `Ron Santos`;
+    const position = `Programmer | QA & BI`
+    const motto = `I code. I automate. I analyze. Solutions that hit hard.`
+
     return (
       <section className="h-full">
         
@@ -14,13 +33,37 @@ const Home = () => {
           <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
             {/* text */}
             <div className="text-center xl:text-left order-2 xl:order-none">
-              <span className="text-xl">Emerging Full-Stack Developer | QA & BI </span>
-              <h1 className="h1 mb-6">
-                Hello I&apos;m <br /> <span className="text-accent">Ron Santos</span>
-              </h1>
-              <p className="max-w-[500px] mb-9 text-white/80">
-                Emerging Full-Stack Developer with a foundation in business Intelligence and QA. Skilled in building portfolio projects using Next.js, React.js, and PHP, while applying QA and automation practice for efficient and reliable development, brings experience in analytics and report automation, supporting both technical delivery and business outcomes, with strong problem-solving, communication, and collaboration skills.
-              </p>
+              <motion.h1
+                className="text-4xl font-bold"
+                initial={{ opacity: 0, y: -20  }}
+                animate={{ opacity: 1, y: 0  }}
+                transition={{ duration: 5, ease: "easeInOut" }}
+              >{text}</motion.h1>
+              
+              <motion.h1 
+                initial={{ opacity: 0, width: 0 }}
+                animate={{ opacity: 1, width: "100%" }}
+                transition={{ duration: 3, ease: "easeInOut"}}
+                className="h1 mb-6 text-accent font-bold whitespace-nowrap overflow-hidden"
+              >
+                {name}
+              </motion.h1>
+              <motion.h1 
+                initial={{ opacity: 0, width: 0 }}
+                animate={{ opacity: 1, width: "100%" }}
+                transition={{ duration: 5, ease: "easeInOut" }}
+                className="text-xl text-accent font-bold whitespace-nowrap overflow-hidden"
+              >
+                {position}
+              </motion.h1>
+              <motion.p
+                initial={{ opacity: 0, width: 0 }}
+                animate={{ opacity: 1, width: "110%",  }}
+                transition={{ duration: 5, ease: "easeInOut" }} 
+                className="max-w-[500px] mb-9 text-white/80 font-bold whitespace-nowrap overflow-hidden"
+              >
+                I code. I automate. I analyze. Make impact.
+              </motion.p>
               {/* buttons and socials */}
               <div className="flex flex-col xl:flex-row items-center gap-8">
                 <a
