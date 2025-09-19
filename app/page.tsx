198ce10248c36f5ecf670d2cd0190a7e6f5fe417
unknown <ron.garcia.santos@gmail.com>
2025-09-20 02:48:21 +0800

    Updating Summary and CV

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ const Home = () => {
                 Hello I&apos;m <br /> <span className="text-accent">Ron Santos</span>
               </h1>
               <p className="max-w-[500px] mb-9 text-white/80">
-                erging Full-Stack Developer with a foundation in business Intelligence and QA. Skilled in building portfolio projects using Next.js, React.js, and PHP, while applying QA and automation practice for efficient and reliable development, brings experience in analytics and report automation, supporting both technical delivery and business outcomes, with strong problem-solving, communication, and collaboration skills.
+                Emerging Full-Stack Developer with a foundation in business Intelligence and QA. Skilled in building portfolio projects using Next.js, React.js, and PHP, while applying QA and automation practice for efficient and reliable development, brings experience in analytics and report automation, supporting both technical delivery and business outcomes, with strong problem-solving, communication, and collaboration skills.
               </p>
               {/* buttons and socials */}
               <div className="flex flex-col xl:flex-row items-center gap-8">
