a52d1829fb377513f8962ebadd8aa3ada1c5aa3c
unknown <ron.garcia.santos@gmail.com>
2025-10-23 18:27:36 +0800

    update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,23 +17,30 @@ export default function ProjectsPage() {
           ease: "easeInOut",
         },
       }}
-      className="bg- white relative min-h-screen w-full text-white px-6 py-16 bg-gradient-to-br from-[#1C1C21] via-[#202A29] to-[#1C1C21]"
+      className="carousel relative min-h-screen w-full text-white px-6 py-16 bg-gradient-to-br from-[#1C1C21] via-[#202A29] to-[#1C1C21]"
     >
-      <motion.div
-        initial={{ opacity: 0, width: 0, }}
-        animate={{ opacity: 1, width: "110%", }}
-        transition={{ duration: 10, ease: "easeInOut" }}
-        className="max-w-[500px] whitespace-nowrap overflow-hidden"
-      >
-        Projects Page Restructure Comming Soon....
-        <ul>
-          {projects.map((project) => {
-            return <li key={project.num}>{project.num}. {project.title}</li>
-          })}
-        </ul>
-      </motion.div>
+      <div id="slide1" className="hidden w-0">
+        <h1>Slide1</h1>
+        <div>
+          <a href="#slide3" className="btn btn-circle">previous</a>
+          <a href="#slide2" className="btn btn-circle">next</a>
+        </div>
+      </div>
+      <div id="slide2" className="hidden">
+        <h1>Slide 2</h1>
+        <div>
+          <a href="#slide1" className="btn btn-circle">previous</a>
+          <a href="#slide3" className="btn btn-circle">next</a>
+        </div>
+      </div>
+      <div id="slide3">
+        <h1>Slide 3</h1>
+        <div>
+          <a href="#slide2" className="btn btn-circle">previous</a>
+          <a href="#slide1" className="btn btn-circle">next</a>
+        </div>
+      </div>
 
-    
     </motion.section>
   );
 }
